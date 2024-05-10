60d5a6a4215811fd4a9321a6e7b30a7796046c1e
webaldar <webaldar@gmail.com>
2024-05-10 16:06:44 +0300

    3 part created

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,50 +1,67 @@
 import {FilterValuesType, TaskType} from "./App";
 import {Button} from "./Button";
-import {useRef} from "react";
+import {ChangeEvent, KeyboardEvent, useState} from "react";
 
 type PropsType = {
-	title: string
-	tasks: TaskType[]
-	removeTask: (taskId: string) => void
-	addTask: (title: string) => void
-	changeFilter: (filter: FilterValuesType) => void
+    title: string
+    tasks: TaskType[]
+    removeTask: (taskId: string) => void
+    addTask: (title: string) => void
+    changeFilter: (filter: FilterValuesType) => void
 }
 
 export const Todolist = ({title, tasks, removeTask, addTask, changeFilter}: PropsType) => {
-	const inputRef = useRef<HTMLInputElement>(null)
-	return (
-		<div>
-			<h3>{title}</h3>
-			<div>
-				<input ref={inputRef}/>
-				<Button onClick={() => {
-					if(inputRef.current){
-						addTask(inputRef.current.value)
-						inputRef.current.value = ""}
-					}}
-					 title={'+'}/>
-			</div>
-			{
-				tasks.length === 0
-					? <p>Тасок нет</p>
-					: <ul>
-						{tasks.map(task => {
-							console.log(task.id)
-							return (
-								<li key={task.id}>
-									<input type="checkbox" checked={task.isDone}/>
-									<span>{task.title}</span>
-									<Button title={'x'} onClick={() => removeTask(task.id)}/>
-								</li>
-							)
-						})}
-					</ul>
-			}
-			<div>
-				<Button title={'All'} onClick={()=> changeFilter('all')}/>
-				<Button title={'Active'} onClick={()=> changeFilter('active')}/>
-				<Button title={'Completed'} onClick={()=> changeFilter('completed')}/>
-			</div>
-		</div>
-	)
+    const [taskTitle, setTaskTitle] = useState('')
+    const addTaskTitletHandler = () => {
+        addTask(taskTitle)
+        setTaskTitle('')
+    }
+    const changeTaskTitletHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setTaskTitle(event.currentTarget.value)
+    }
+    const addTaskOnKeyUpHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') {
+            addTaskTitletHandler()
+        }
+    }
+    const changeFilterTaskHundler = (filter: FilterValuesType) => {
+        changeFilter(filter)
+    }
+    return (
+        <div>
+            <h3>{title}</h3>
+            <div>
+                <input value={taskTitle}
+                       onChange={changeTaskTitletHandler}
+                       onKeyUp={addTaskOnKeyUpHandler}/>
+                <Button onClick={addTaskTitletHandler}
+                        title={'+'}/>
+            </div>
+            {
+                tasks.length === 0
+                    ? <p>Тасок нет</p>
+                    : <ul>
+                        {
+                            tasks.map(task => {
+								const removeTaskHundler = () => {
+                                    removeTask(task.id)
+                                }
+                                return (
+                                    <li key={task.id}>
+                                        <input type="checkbox" checked={task.isDone}/>
+                                        <span>{task.title}</span>
+                                        <Button title={'x'} onClick={() => removeTaskHundler()}/>
+                                    </li>
+                                )
+                            })
+						}
+                    </ul>
+            }
+            <div>
+                <Button title={'All'} onClick={() => changeFilterTaskHundler('all')}/>
+                <Button title={'Active'} onClick={() => changeFilterTaskHundler('active')}/>
+                <Button title={'Completed'} onClick={() => changeFilterTaskHundler('completed')}/>
+            </div>
+        </div>
+    )
 }
